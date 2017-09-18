9cf5a038e2a80857eee0b41897cbda485a500139
Brian Mann <bmann505@gamil.com>
2017-09-18 11:15:06 -0600

    deploy

--- battle-codes-API/knexfile.js
+++ battle-codes-API/knexfile.js
@@ -2,5 +2,9 @@ module.exports = {
   development: {
     client: 'pg',
     connection: 'postgres://localhost/battle_code'
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 };
